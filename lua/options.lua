9fa474662c8c2cdfccf9e24c3600e5147dc20cc7
stefiii <stefanundervover20@gmail.com>
2024-05-11 17:42:30 +0200

    startpage oida

--- lua/options.lua
+++ lua/options.lua
@@ -56,4 +56,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- let g:
+vimtex_view_method = 'zathura'
 -- vim: ts=2 sts=2 sw=2 et
+vim.cmd("colorscheme rose-pine-moon")
